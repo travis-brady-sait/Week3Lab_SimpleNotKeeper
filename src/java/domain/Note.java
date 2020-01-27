b634263bb71c11acd6d186d9e64b087092274469
travis brady <travis.brady@edu.sait.ca>
2020-01-27 12:51:22 -0700

    initial commit

---
+++ src/java/domain/Note.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 798794
+ */
+public class Note implements Serializable{
+    
+    private String content;
+    private String title;
+
+    public Note(){
+        title = "";
+        content = "";
+    }
+
+    public Note(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+}
---
+++ web/WEB-INF/viewnote.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : viewnote
+    Created on : Jan 27, 2020, 12:38:22 PM
+    Author     : 798794
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
